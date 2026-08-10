e5fbe8c2eb534819bc28fa29d9c8166d69115a33
JulioTrini17 <trinidadgarciajuliocesar@gmail.com>
2026-08-09 23:19:20 -0600

    ultimo cambio en conexion

--- php/conexion.php
+++ php/conexion.php
@@ -2,9 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 $host = "localhost";
-$db = "nahui";
-$user = 'root';
-$pass = "260622BrunX";
+$db   = "aulanet_dev";
+$user = "aulanet_local";
+$pass = "Aul@NetLocal2026!";
 
 try {
     $pdo = new PDO(
